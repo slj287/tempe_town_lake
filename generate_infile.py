ef9ddd79d0c0a487b0257e5ae9ca298319436806
Lionel D. Hummel <lionel@ieee.org>
2018-06-07 13:06:07 -0700

    generate_infile: generate_for_station to use narrow_asos_df_to_valid_hourly

--- generate_infile.py
+++ generate_infile.py
@@ -5,8 +5,8 @@ import sys
 import pandas as pd
 
 from data_sources import in_out_file_map
+from wind_analysis import narrow_asos_df_to_valid_hourly
 
-indexable_timestamp = True
 
 def generate_for_station(station_sym="PHX"):
 
@@ -15,24 +15,8 @@ def generate_for_station(station_sym="PHX"):
     print("> Load raw readings from %s" % (infile))
     df = load_asos(infile, index_col=index_col_for_this_file)
 
-    if indexable_timestamp:
-        df["timestamp"] = pd.DatetimeIndex(df.index)
-    else:
-        df["timestamp"] = df.index
 
-    print("> Narrowing to timestamp, drct, and sknt (+ dropping NaNs)")
-    phx_df = df[df.sknt.notna() & df.drct.notna()][['timestamp', 'drct', 'sknt']]
-
-    print("> Dedup readings")
-    deduped_group = dedup_readings(phx_df)
-
-    if "Hourly" in deduped_group.columns:
-        deduped_group.index = deduped_group.Hourly
-
-    cols_to_drop = (set(deduped_group.columns)
-                    .intersection(set(["Hourly", "Hourly.1", "OffsetFromHour"])))
-    if cols_to_drop:
-        deduped_group.drop(cols_to_drop, axis=1, inplace=True)
+    deduped_group = narrow_asos_df_to_valid_hourly(df)
 
     deduped_group.to_csv(outfile)
     print("> You now have a sparse set of hourly readings in %s." % (outfile))
