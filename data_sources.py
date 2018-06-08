c3e82354876a2d4e4c19146b5f212ee4478872d2
Lionel D. Hummel <lionel@ieee.org>
2018-06-08 08:29:15 -0700

    Raw ASOS and ddg: Williams Gateway: IWA_199301010000_201806080051.txt

--- data_sources.py
+++ data_sources.py
@@ -2,6 +2,7 @@
 
 in_out_file_map = {
     #        infile,      index_col_for_this_file,  outfile
+    "IWA": ("IWA_199301010000_201806080051.txt", 1, "ddg-iwa.csv"),
     #"PHX": ("phx_through_2017-12-20-23:55.csv", 0, "ddg-phx.csv"),
     "PHX": ("PHX_199301010000_201711211517.txt", 1, "ddg.csv"),
     "FFZ": ("FFZ_199301010000_201711211517.txt", 1, "ddg-ffz.csv"),
