59c44eddfc9fc701fdff187bcec74e2a4ba94678
Lionel D. Hummel <lionel@ieee.org>
2018-06-08 08:21:12 -0700

    Re-generate NaN-less ddg.csv with PHX_199301010000_201711211517.txt

--- data_sources.py
+++ data_sources.py
@@ -2,8 +2,8 @@
 
 in_out_file_map = {
     #        infile,      index_col_for_this_file,  outfile
-    "PHX": ("phx_through_2017-12-20-23:55.csv", 0, "ddg.csv"),
-    "PHX-orig": ("../PHX_199301010000_201711211517.txt", 1, "ddg-phx.csv"),
+    #"PHX": ("phx_through_2017-12-20-23:55.csv", 0, "ddg-phx.csv"),
+    "PHX": ("PHX_199301010000_201711211517.txt", 1, "ddg.csv"),
     "FFZ": ("FFZ_199301010000_201711211517.txt", 1, "ddg-ffz.csv"),
     "SDL": ("SDL_199301010000_201711211517.txt", 1, "ddg-sdl.csv"),
 }
